ed1a8215e3f11bc2846e5cb4f591ff4e9102f1a7
Serena Chuang <tzyharnchuang@gmail.com>
2024-04-07 12:04:45 -0700

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import styles from './page.module.scss';
 import Image from 'next/image';
+import OutreachLogo from "@/public/outreach-logo.svg";
+
+// breakpoint = 870px
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
-        <Image src="/acm-logo.png" width={100} height={100} alt="ACM Logo" />
-      </div>
-      <div>
-        <h1>Welcome to ACM&apos;s static site template!</h1>
+      <div className={styles.landing}>
+        <div className={styles.landingContent}>
+          <div className={styles.outreachDesc}>
+            <h1>ACM Outreach at UCSD</h1>
+            <p>Welcome to ACM Outreach at UCSD, where our mission is to ignite a passion for computing and STEM among K-12 students, with a special focus on those who are underrepresented in the computing community. Our goal is to empower students to explore the exciting world of computing by nurturing their curiosity and enthusiasm for technology. Join us in inspiring the next generation of innovators and problem solvers! </p>
+          </div>
+          <div className={styles.outreachLogo}>
+            <Image src="/outreach-logo.svg" width={250} height={250} alt="Outreach Logo" />
+          </div>
+        </div>
       </div>
     </main>
   );
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,29 @@
+<<<<<<< Updated upstream
 import Image from 'next/image';
 import styles from './style.module.scss';
 import Typography from '../Typography';
 import InstagramIcon from '@/public/instagram-logo.svg';
+=======
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./styles.module.scss";
+import MailIcon from "@/public/mail-icon.svg";
+import InstagramLogo from "@/public/instagram-logo.svg";
+import DiscordIcon from "@/public/discord-icon.svg";
+
+>>>>>>> Stashed changes
 
 const Footer: React.FC = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const notMobile = styles[`mobile${!mobileOpen ? '_show': ''}`]
+  const mobile = styles[`mobile${mobileOpen ? '_show': ''}`]
+
   return (
     <div className={styles.footer}>
       <div className={styles.footerContent}>
         <div className={styles.findUs}>
+<<<<<<< Updated upstream
           <Typography variant="display/medium">Find us here!</Typography>
           <div className={styles.description}>
             <Typography variant="subheading">Interested in learning more?</Typography>
@@ -16,6 +32,11 @@ const Footer: React.FC = () => {
               community!
             </Typography>
           </div>
+=======
+          <h1>Find us here!</h1>
+          <p>Interested in learning more?</p>
+          <p>Reach out to get connected, or explore our socials to get a sense of who we are as a community!</p>
+>>>>>>> Stashed changes
         </div>
         <div className={styles.socials}>
           <div className={styles.socialInfo}>
@@ -25,7 +46,11 @@ const Footer: React.FC = () => {
               Send us an inquiry
             </Typography>
           </div>
+<<<<<<< Updated upstream
 
+=======
+          
+>>>>>>> Stashed changes
           <div className={styles.socialInfo}>
             <Image src="/instagram-logo.svg" width={50} height={50} alt="Instagram Logo" />
             {/* <InstagramLogo /> */}
@@ -33,7 +58,11 @@ const Footer: React.FC = () => {
               Follow us for updates
             </Typography>
           </div>
+<<<<<<< Updated upstream
 
+=======
+          
+>>>>>>> Stashed changes
           <div className={styles.socialInfo}>
             <Image src="/discord-icon.svg" width={50} height={50} alt="Discord Icon" />
             <Typography variant="subheading" className={styles.socialText}>

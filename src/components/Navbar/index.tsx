d6ac6b8f89bd9d1af4bbffcef0b55e2fa497cc46
Kevin12J <143035696+Kevin12J@users.noreply.github.com>
2026-05-15 17:54:21 -0700

    Navbar logo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -13,10 +13,15 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
-        <a href="#hero" className={styles.logo}>
-            {/* <Image src={'/outreach-logo.svg'} alt='Outreach' width={100} height={100} /> */}
-            ACM Outreach
-        </a>
+        <div className={styles.logoContainer}>
+          <a href="#hero" className={styles.icon}>
+            <Image src="/outreach-logo.svg" alt="Outreach" fill sizes="40px" style={{ objectFit: 'contain' }} />
+          </a>
+          <div className={styles.logoText}>
+            <span className={styles.logoTitle}>Outreach</span>
+            <span className={styles.logoSubtitle}>ACM at UC San Diego</span>
+          </div>
+        </div>
         <ul className={styles.navLinks}>
           {links.map((link) => (
             <li key={link.href}>
